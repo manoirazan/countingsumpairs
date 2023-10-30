d166407b8df2c3b09561ae8f7f0940649a181f40
manochernazari <manocher.nazari@puregym.com>
2023-10-30 00:48:03 +0000

    completed

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace CountingSumPairs
+{
+    class Pogram
+    {
+        public static void CountPairsWithSum(int[] arrayOfNumbers, int X)
+        {
+            int count = 0; 
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+                for (int j = i + 1; j < arrayOfNumbers.Length; j++)
+                    if ((arrayOfNumbers[i] + arrayOfNumbers[j]) == X)
+                        count++;
+
+            Console.WriteLine($"X:{X}");
+            Console.WriteLine($"Array:{String.Join(",",arrayOfNumbers)}");
+            Console.WriteLine($"Output:{count}");
+        }
+
+        static public void Main()
+        {
+            int[] arrayOfNumbers = { 3, 4, 5, 6};
+            int x = 1;           
+            CountPairsWithSum(arrayOfNumbers, x);
+
+            int[] arrayOfNumberTwo = { 0, 15, 32, 2000,15000 };
+            int xx = 15;
+            CountPairsWithSum(arrayOfNumberTwo, xx);
+
+
+            int[] arrayOfNumbersThree = { 1, 1, 10, 32, 41 };
+            int xxx = 42;
+            CountPairsWithSum(arrayOfNumbersThree, xxx);
+
+        }
+    }
+}
+
